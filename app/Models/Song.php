7167375ac56ba6eeb8e81460313559136c220b49
Mamenosaibanjin <f.gill@4-go.de>
2025-04-02 15:51:21 +0200

    "Erstelle Song Model mit Beziehungen zu Playlist und AudioFeature

---
+++ app/Models/Song.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Song extends Model
+{
+    use HasFactory;
+    
+    /**
+     * Die Tabelle, die mit diesem Model verbunden ist.
+     *
+     * @var string
+     */
+    protected $table = 'songs';
+    
+    /**
+     * Die Attribute, die massenweise zuweisbar sind.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'artist',
+        'album',
+        'duration',
+    ];
+    
+    /**
+     * Gibt die Playlists zurück, in denen dieser Song enthalten ist.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function playlists()
+    {
+        return $this->belongsToMany(Playlist::class, 'playlist_song')
+        ->withTimestamps();
+    }
+    
+    /**
+     * Gibt die Audio-Features für diesen Song zurück.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function audioFeature()
+    {
+        return $this->hasOne(SongAudioFeature::class);
+    }
+}
