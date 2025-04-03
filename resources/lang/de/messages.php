e278d485b22c25f1587e086033ad1b405c9ccb90
Mamenosaibanjin <f.gill@4-go.de>
2025-04-03 05:19:28 +0200

    Spracheinstellungen, Anpassung der Login-Maske

---
+++ resources/lang/de/messages.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'email_address' => 'E-Mail-Adresse',
+    'password' => 'Passwort',
+    'remember_me' => 'Angemeldet bleiben',
+    'login' => 'Einloggen',
+    'register' => 'Registrieren',
+    'forgot_password' => 'Passwort vergessen?',
+];
