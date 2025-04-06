243143ba0c05aec5f9d3930f950fcec30c184a49
Mamenosaibanjin <f.gill@4-go.de>
2025-04-06 19:43:02 +0200

    Fehlerhafte Authentifizierung gefixt, Redirects angepasst

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -27,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/playlists';
+    protected $redirectTo = '/home';
 
     /**
      * Create a new controller instance.
@@ -38,17 +36,5 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
         $this->middleware('auth')->only('logout');
-    }
-    
-    public function logout(Request $request)
-    {
-        Auth::logout();
-        $request->session()->invalidate();
-        $request->session()->regenerateToken();
-        
-        // Nachricht in der Session setzen
-        $request->session()->flash('status', 'Sie wurden erfolgreich abgemeldet.');
-        
-        return redirect('/login'); 
     }
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,14 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-                @if(session('status'))
-                    <div class="alert alert-warning">
-                        {{ session('status') }}
-                    </div>
-                @endif
-                
-                            <div class="card">
-
+            <div class="card">
                 <div class="card-header">{{ __('Login') }}</div>
 
                 <div class="card-body">
